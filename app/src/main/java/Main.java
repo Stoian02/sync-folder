539722cfce5f082e0c38db049e85cc0d6d186f27
Stoian02 <stoianstoikov@gmail.com>
2025-07-27 22:39:46 +0300

    Add test code to test the diff engine

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import core.DirectoryScanner;
 import core.FileInfo;
+import util.DiffEngine;
 
 import java.nio.file.Path;
 import java.util.Map;
@@ -8,15 +9,23 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Directory Scanner Application");
         
-        if (args.length < 1) {
-            System.out.println("Incorrect usage, do: java Main <directory-path>");
+        String[] args_2 = {"./test", "./target"};
+        
+        if (args_2.length < 2) {
+            System.out.println("Incorrect usage, do: java Main <source-path> <target-path>");
             return;
         }
 
-        Path path = Path.of(args[0]);
-        Map<String, FileInfo> result = DirectoryScanner.scan(path);
+        Path path_1 = Path.of(args_2[0]);
+        Path path_2 = Path.of(args_2[1]);
+        // Map<String, FileInfo> result = DirectoryScanner.scan(path);
 
-        System.out.println("Scanned files:");
-        result.values().forEach(System.out::println);
+        // System.out.println("Scanned files:");
+        // result.values().forEach(System.out::println);
+
+        Map<String, FileInfo> source = DirectoryScanner.scan(path_1);
+        Map<String, FileInfo> target = DirectoryScanner.scan(path_2);
+
+        DiffEngine.compare(source, target);
     }
 }
