1eb6ca649e12c2b5c21343a8a197c5c11bac8ed0
Stoian02 <stoianstoikov@gmail.com>
2025-07-30 09:36:22 +0300

    Refactored main to use the command class cli/SyncFolder.java and removed unecessary imports

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,32 +1,17 @@
-import core.DirectoryScanner;
-import core.FileInfo;
-import core.SyncExecutor;
-import core.SyncOperation;
-import util.DiffEngine;
+import picocli.CommandLine;
 
-import java.nio.file.Path;
-import java.util.List;
-import java.util.Map;
+import cli.SyncFolder;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Directory Scanner Application");
-        
-        String[] args_2 = {"./test", "./target"};
-        
-        if (args_2.length < 2) {
+
+        if (args.length < 2) {
             System.out.println("Incorrect usage, do: java Main <source-path> <target-path>");
             return;
         }
 
-        Path path_1 = Path.of(args_2[0]);
-        Path path_2 = Path.of(args_2[1]);
-
-        Map<String, FileInfo> source = DirectoryScanner.scan(path_1);
-        Map<String, FileInfo> target = DirectoryScanner.scan(path_2);
-
-        List<SyncOperation> operations = DiffEngine.compare(source, target);
-
-        SyncExecutor.execute(operations, path_1, path_2, true);
+        int exitCode = new CommandLine(new SyncFolder()).execute(args);
+        System.exit(exitCode);
     }
 }
